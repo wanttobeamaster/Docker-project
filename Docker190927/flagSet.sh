f26606ab43776d2d2753bb6d1bce9fe72da1681e
XiaoZhiqing <xiaozhiqing@localhost.localdomain>
2019-09-27 15:22:09 +0800

    3

--- Docker190927/flagSet.sh
+++ Docker190927/flagSet.sh
@@ -2,13 +2,12 @@
 redis-server --daemonize yes
 service ssh start
 flagCont=$(date +%s%N|cut -c9-16)
-echo $flagCont > /root/flag.txt
-pyinstaller -F God.py
+echo $flagCont > /home/flag/flag.txt
+chown flag:flag /home/flag/flag.txt
+chmod 770 /home/flag/flag.txt
 chmod 777 /home/test
-chmod 777 /home/test/dist/God
-chmod u+s /home/test/dist/God
-rm -f God.py
-chmod 000 /home/test/flag.txt
+cat /etc/passwd | grep flag > /home/test/passwd.txt
+cat /etc/shadow | grep flag > /home/test/shadow.txt
 
 connectRes=$(redis-cli -h $1 -p $2 -a $3 set "container_flag_$4_$5" $flagCont)
 if [ "$connectRes" = 'OK' ]
